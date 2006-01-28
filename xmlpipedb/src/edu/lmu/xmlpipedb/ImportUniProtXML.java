875fecdf1c096c647848d792b486d53b0ec8354d
dondi <dondi@lmu.edu>
2006-01-28 01:00:55 +0000

    First boostrap commit!

---
+++ xmlpipedb/src/edu/lmu/xmlpipedb/ImportUniProtXML.java
@@ -0,0 +1,37 @@
+/*
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  Web: http://sourceforge.net/projects/xmlpipedb
+*/
+// Created by dondi, Jan 27, 2006.
+package edu.lmu.xmlpipedb;
+
+/**
+ * ImportUniProtXML imports an XML file based on UniProt's XML Schema definition
+ * into a relational database.
+ * 
+ * @author dondi
+ * @version $Revision$ $Date$
+ */
+public class ImportUniProtXML {
+    /**
+     * The execution entry point for the utility.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Write this utility when everything else is ready.
+    }
+}

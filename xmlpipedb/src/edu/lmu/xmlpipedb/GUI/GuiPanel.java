87d60bb73f81b4647261c1e1e8419ad53801c17e
rtoal <rtoal@lmu.edu>
2006-02-10 03:23:48 +0000

    Pretty up the layout

--- xmlpipedb/src/edu/lmu/xmlpipedb/GUI/GuiPanel.java
+++ xmlpipedb/src/edu/lmu/xmlpipedb/GUI/GuiPanel.java
@@ -1,19 +1,19 @@
 /*
- * guiPanel.java
+ * GuiPanel.java
  *
  * Created on February 8, 2006, 9:29 AM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
  */
 
 package edu.lmu.xmlpipedb.GUI;
 
-import edu.lmu.xmlpipedb.GUI.util.JLabelComponentFactory;
-import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
+
 import javax.swing.AbstractAction;
-import javax.swing.Box;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -22,128 +22,130 @@ import javax.swing.JPasswordField;
 import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 
+import edu.lmu.xmlpipedb.GUI.util.JLabelComponentFactory;
+
 /**
+ * A simple login panel.
  *
- * @author Administrator
+ * @author David Hoffman
  */
 public class GuiPanel extends JPanel{
-    
-    
-    private AbstractAction _startButtonAction; 
-    private JButton _loadXmlToDatabaseButton; 
+
+    private AbstractAction _startButtonAction;
+    private JButton _loadXmlToDatabaseButton;
     private JTextField _xmlFileBox;
-    private JTextField _dataBaseNameBox; 
+    private JTextField _dataBaseNameBox;
     private JTextField _dataBaseUserBox;
-    private JPasswordField _dataBasePasswordBox; 
-    private JProgressBar _xmlLoadProgress; 
-    private Gui _parent; 
-    
-    
+    private JPasswordField _dataBasePasswordBox;
+    private JProgressBar _xmlLoadProgress;
+    private Gui _parent;
+
+    /**
+     * Creates the panel.
+     *
+     * @param parent
+     */
     public GuiPanel(Gui parent) {
-        _parent = parent; 
+        _parent = parent;
 
         createActions();
         createComponents();
-        layoutComponents(); 
+        layoutComponents();
     }
-    
-    public void setXmlFilePath(String path)
-    {
-        _xmlFileBox.setText(path); 
+
+    public void setXmlFilePath(String path) {
+        _xmlFileBox.setText(path);
     }
-    private void createActions()
-    {
-        createStartButtonAction(); 
+
+    private void createActions() {
+        createStartButtonAction();
     }
-    private void createStartButtonAction()
-    {
-       
-        final GuiPanel me = this;  
+
+    private void createStartButtonAction() {
+
+        final GuiPanel me = this;
         _startButtonAction = new AbstractAction(){
             public void actionPerformed(ActionEvent event)
             {
-                
+
                 if(!isThereDataInTheFields())
                 {
-                    JOptionPane.showMessageDialog(me, "All fields are required", "Input Needed", JOptionPane.ERROR_MESSAGE); 
-                    return; 
+                    JOptionPane.showMessageDialog(me, "All fields are required", "Input Needed", JOptionPane.ERROR_MESSAGE);
+                    return;
                 }
                 try{
-                    _parent.startXmlToDataBase( _xmlFileBox.getText() ); 
+                    _parent.startXmlToDataBase( _xmlFileBox.getText() );
                 }
                 catch(Exception e){
-                    JOptionPane.showMessageDialog(me, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE); 
+                    JOptionPane.showMessageDialog(me, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
-        };        
-    }
-    private void createComponents()
-    {
-        _loadXmlToDatabaseButton = new JButton(_startButtonAction); 
-        _loadXmlToDatabaseButton.setText("Start"); 
-        _dataBaseNameBox = new JTextField(); 
-        _dataBaseUserBox = new JTextField(); 
-        _dataBasePasswordBox = new JPasswordField();  
-        _xmlFileBox = new JTextField(); 
-        _xmlLoadProgress = new JProgressBar();
-        
+        };
     }
 
-    private void layoutComponents()
-    {
-        //SpringLayout layout = new SpringLayout(); 
-        //BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS); 
-        BorderLayout layout = new BorderLayout(5,5); 
-        setLayout(layout); 
-        JLabel xmlLabel = new JLabel("XML File Path: "); 
-        JLabel nameLabel = new JLabel("Database Name: "); 
-        JLabel userLabel = new JLabel("Database User: ");
-        JLabel passwordLabel = new JLabel("Database Password: ");
-        
-        Box label = Box.createVerticalBox();
-        label.add(Box.createVerticalGlue()); 
-        label.add(xmlLabel); 
-        label.add(Box.createVerticalGlue()); 
-        label.add(nameLabel); 
-        label.add(Box.createVerticalGlue()); 
-        label.add(userLabel); 
-        label.add(Box.createVerticalGlue()); 
-        label.add(passwordLabel); 
-        label.add(Box.createVerticalGlue()); 
-        add(label, BorderLayout.WEST); 
-        
-        Box text = Box.createVerticalBox(); 
-        text.add(Box.createVerticalGlue()); 
-        text.add(_xmlFileBox); 
-        text.add(Box.createVerticalGlue()); 
-        text.add(_dataBaseNameBox); 
-        text.add(Box.createVerticalGlue()); 
-        text.add(_dataBaseUserBox); 
-        text.add(Box.createVerticalGlue()); 
-        text.add(_dataBasePasswordBox); 
-        text.add(Box.createVerticalGlue()); 
-        add(text, BorderLayout.CENTER); 
-        
-        Box buttonBox = Box.createHorizontalBox(); 
-        buttonBox.add(Box.createHorizontalGlue());
-        buttonBox.add(_loadXmlToDatabaseButton); 
-        buttonBox.add(Box.createHorizontalStrut(5)); 
-        
-        Box southBox = Box.createVerticalBox(); 
-        southBox.add(Box.createVerticalGlue()); 
-        southBox.add(JLabelComponentFactory.createHorizontalLabelComponent("Progress: ", 5, _xmlLoadProgress)); 
-        southBox.add(Box.createVerticalGlue()); 
-        southBox.add(buttonBox); 
-        southBox.add(Box.createVerticalGlue()); 
-        add( southBox, BorderLayout.SOUTH);
-    
+    private void createComponents() {
+        _loadXmlToDatabaseButton = new JButton(_startButtonAction);
+        _loadXmlToDatabaseButton.setText("Start");
+        _dataBaseNameBox = new JTextField(30);
+        _dataBaseUserBox = new JTextField(30);
+        _dataBasePasswordBox = new JPasswordField(30);
+        _xmlFileBox = new JTextField(30);
+        _xmlLoadProgress = new JProgressBar();
     }
-    private boolean isThereDataInTheFields()
-    {
-    	//BN - the command line entry point only takes an XML file, no DB information
-    	return _xmlFileBox.getText().length() > 0;
-       
-        //return (_xmlFileBox.getText().length()>0)&&(_dataBaseNameBox.getText().length()>0)&&(_dataBaseUserBox.getText().length()>0)&& (_dataBasePasswordBox.getPassword().length>0); 
+
+    private void layoutComponents() {
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        setLayout(new GridBagLayout());
+        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+        JPanel progressPanel = new JPanel();
+        progressPanel.add(JLabelComponentFactory
+                .createHorizontalLabelComponent(
+                        "Progress: ", 5, _xmlLoadProgress));
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+        buttonPanel.add(_loadXmlToDatabaseButton);
+
+        // Text field labels
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        add(new JLabel("XML File Path: "), gbc);
+        gbc.gridy++;
+        add(new JLabel("Database Name: "), gbc);
+        gbc.gridy++;
+        add(new JLabel("Database User: "), gbc);
+        gbc.gridy++;
+        add(new JLabel("Database Password: "), gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0;
+        add(_xmlFileBox, gbc);
+        gbc.gridy++;
+        add(_dataBaseNameBox, gbc);
+        gbc.gridy++;
+        add(_dataBaseUserBox, gbc);
+        gbc.gridy++;
+        add(_dataBasePasswordBox, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridwidth = 2;
+        gbc.insets = new Insets(10, 0, 0, 0);
+        add(progressPanel, gbc);
+
+        gbc.gridy++;
+        add(buttonPanel, gbc);
     }
-    
+
+    private boolean isThereDataInTheFields() {
+        //BN - the command line entry point only takes an XML file, no DB information
+        return _xmlFileBox.getText().length() > 0;
+
+        //return (_xmlFileBox.getText().length()>0)&&(_dataBaseNameBox.getText().length()>0)&&(_dataBaseUserBox.getText().length()>0)&& (_dataBasePasswordBox.getPassword().length>0);
+    }
 }

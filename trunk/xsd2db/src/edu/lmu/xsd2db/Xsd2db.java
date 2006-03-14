d43d963adc80e88f6d4c375ecffab20cc8136a9f
vespasian10 <vespasian10@1094f71c-c033-0410-a037-bb4d46bb0a42>
2006-03-14 21:17:18 +0000

    Initial upload

---
+++ trunk/xsd2db/src/edu/lmu/xsd2db/Xsd2db.java
@@ -0,0 +1,18 @@
+
+
+public class Xsd2db {
+    /**
+     * The execution entry point for the utility.
+     *
+     * @param args
+     */
+	public static void main(String[] args)  {
+		Xsd2dbCommandLine cmdline = new Xsd2dbCommandLine();
+		cmdline.parse(args);
+	}
+
+}
+
+
+
+

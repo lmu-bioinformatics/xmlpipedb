180e4164c03384ff6e89a609fffdaf8ecdda2951
joeyjbarrett <joeyjbarrett@1094f71c-c033-0410-a037-bb4d46bb0a42>
2006-04-13 09:30:23 +0000

    changed package name to databasetoolkit. --Joey J. Barrett

---
+++ trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/ExportToGenMaPP.java
@@ -0,0 +1,193 @@
+package edu.lmu.xmlpipedb.gmbuilder.databasetoolkit;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class ExportToGenMaPP {
+
+	private Connection connection = null;
+	private String outputFile = null;
+	
+	public ExportToGenMaPP(String templateFile, String outputFile) throws ClassNotFoundException, SQLException, IOException {
+		
+		this.outputFile = outputFile;
+		
+		//make a copy of the template file.
+		copyFile(new File(templateFile), new File(outputFile));
+	}
+	
+	private void copyFile(File fileIn, File fileOut) throws IOException {
+		InputStream in = new FileInputStream(fileIn);
+		OutputStream out = new FileOutputStream(fileOut);
+	    
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+    }
+	
+	public void openConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+		   
+        String database = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};DBQ=";
+        database += outputFile.trim() + ";DriverID=22;READONLY=false}"; 
+        
+        connection = DriverManager.getConnection(database ,"","");
+	}
+	
+	public void closeConnection() throws SQLException {
+		connection.close();
+	}
+	
+	public void updateInfoTable(String owner, 
+			String version, String modSystem, String species,
+			String modify, String displayOrder, 
+			String notes) throws SQLException {
+		
+        Statement s = connection.createStatement();
+        s.execute("INSERT INTO Info (" +
+        		"Owner," +
+        		"Version," +
+        		"MODSystem," +
+        		"Species," +
+        		"Modify," +
+        		"DisplayOrder," +
+        		"Notes) " +
+        		"VALUES (" +
+        		"'" + owner + "'," +
+        		"'" + version + "'," +
+        		"'" + modSystem + "'," +
+        		"'" + species + "'," +
+        		"'" + modify + "'," +
+        		"'" + displayOrder + "'," +
+        		"'" + notes + "')");
+        s.close();
+	}
+	
+	public void createUniProtTable() throws SQLException {
+		
+        Statement s = connection.createStatement();
+        s.execute("CREATE TABLE UniProt (" +
+        		"ID VARCHAR(50) NOT NULL," +
+        		"EntryName VARCHAR(50) NOT NULL," +
+        		"GeneName VARCHAR(50) NOT NULL," +
+        		"ProteinName MEMO," +
+        		"Function MEMO," +
+        		"Species MEMO," +
+        		"\"Date\" DATE," +
+        		"Remarks MEMO)");
+        s.execute("ALTER TABLE UniProt ADD CONSTRAINT UniProt_constraint PRIMARY KEY(ID)"); 
+
+        s.close();
+	}
+	
+    public void fillUniProtTable(String id, 
+    			String entryName, String geneName, String proteinName, 
+    			String function, String species, String date, 
+    			String remarks) throws SQLException {
+    	
+    	Statement s = connection.createStatement();
+        s.execute("INSERT INTO UniProt (" +
+        		"ID," +
+        		"EntryName," +
+        		"GeneName," +
+        		"ProteinName," +
+        		"Function," +
+        		"Species," +
+        		"\"Date\"," +
+        		"Remarks)" +
+        		"VALUES (" +
+        		"'" + id + "'," +
+        		"'" + entryName + "'," +
+        		"'" + geneName + "'," +
+        		"'" + proteinName + "'," +
+        		"'" + function + "'," +
+        		"'" + species + "'," +
+        		"'04/06/2006'," +
+        		"'" + remarks + "')");
+        
+        s.close();
+	}
+	
+	public void createAndFillSystemTable(String table, 
+			List<String> idList, String species, String date, 
+			String remarks) throws SQLException {
+        
+		Statement s = connection.createStatement();
+
+        s.execute("CREATE TABLE " + table +" (" +
+        		"ID VARCHAR(50) NOT NULL," +
+        		"Species MEMO," +
+        		"Date DATE," +
+        		"Remarks MEMO)");
+        
+        s.execute("ALTER TABLE " + table + " ADD CONSTRAINT " + table + "_constraint PRIMARY KEY(ID)"); 
+        
+        for(String id : idList) {
+            s.execute("INSERT INTO " + table +" (" +
+            		"ID," +
+            		"Species," +
+            		"Date," +
+            		"Remarks)" +
+            		"VALUES (" +
+            		"'" + id + "'," +
+            		"'" + species + "'," +
+            		"'" + date + "'," +
+            		"'" + remarks + "')");
+        }
+        s.close();
+	}
+	
+	public void createAndFillRelationTable(String table, 
+			String primary, List<String> relatedList, 
+			String bridge) throws SQLException {
+        
+		Statement s = connection.createStatement();
+		
+        s.execute("CREATE TABLE " + table +" (" +
+        		"Primary VARCHAR(50) NOT NULL," +
+        		"Related VARCHAR(50) NOT NULL," +
+        		"Bridge VARCHAR(3) NOT NULL)");
+        
+        //Primary and related are foriegn keys, the bridge is the value S
+       // s.execute("ALTER TABLE " + table + " ADD CONSTRAINT " + table + "_constraint PRIMARY KEY(ID)"); 
+
+        for(String related : relatedList) {
+            s.execute("CREATE TABLE " + table +" (" +
+            		"Primary," +
+            		"Related," +
+            		"Bridge)" +
+            		"VALUES (" +
+            		"'" + primary + "'," +
+            		"'" + related + "'," +
+            		"'" + bridge + "')");
+        }
+        s.close();
+	}
+	
+	public void updateSystemsTable(
+			List<String> systemCodeList, String date) throws SQLException {
+        
+		Statement s = connection.createStatement();
+		
+		for(String systemCode : systemCodeList) {
+            s.execute("UPDATE Systems " +
+            		"SET \"Date\"='04/06/2006'" +
+            		"WHERE SystemCode='" + systemCode + "'");
+		}
+        s.close();
+	}
+}
---
+++ trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/ExtractFromDB.java
@@ -0,0 +1,246 @@
+package edu.lmu.xmlpipedb.gmbuilder.databasetoolkit;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.Session;
+
+
+
+public class ExtractFromDB {
+
+	
+	private Map<String, String> uniprotTable_ID = new HashMap<String, String>();
+	private Map<String, String> uniprotTable_EntryName = new HashMap<String, String>();
+	private Map<String, String> uniprotTable_GeneName = new HashMap<String, String>();
+	private Map<String, String> uniprotTable_ProteinName = new HashMap<String, String>();
+	
+	private Connection connection = null;
+	
+	public ExtractFromDB() {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		   
+        String database = "jdbc:postgresql://database"; 
+        
+        try {
+			connection = DriverManager.getConnection("jdbc:postgresql://database"
+					,"jjbarret","tu00ylyI");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+		
+		
+	public void ExtractUniprotTableData() throws SQLException {
+		
+        Statement s = connection.createStatement();
+        
+        //
+        //  uniprotTable_ID <UID> <Uniprot Table ID>
+        //
+        
+        ResultSet result = s.executeQuery("SELECT " +
+        		"entrytype_accession_hjid, " +
+        		"hjvalue " +
+        		"FROM entrytype_accession");
+        
+        while(result.next()) {
+        	uniprotTable_ID.put(result.getString(1), result.getString(2));
+        }
+        
+
+        for(String id : uniprotTable_ID.keySet()) {
+ 
+            //
+            //  uniprotTable_EntryName <UID> <Uniprot Table EntryName>
+            //
+            
+	        result = s.executeQuery("SELECT hjvalue " +
+	        		"FROM entrytype_name " +
+	        		"WHERE entrytype_name_hjid='" + id + "'");
+	        while(result.next()) {
+	        	uniprotTable_EntryName.put(id, result.getString(1));
+	        	System.out.println("FOUND: " + result.getString(1));
+	        }
+	        
+	        //
+	        //  uniprotTable_GeneName <UID> <Uniprot Table GeneName>
+	        //
+	       
+	        result = s.executeQuery("SELECT value " +
+	        		"FROM genenametype " +
+	        		"WHERE entrytype_genetype_name_hjid='" + id + "' " +
+	        		"AND type='ordered locus'");
+	        while(result.next()) {
+	        	uniprotTable_GeneName.put(id, result.getString(1));
+	        	System.out.println("FOUND: " + result.getString(1));
+	        }
+	        
+	        //
+	        //  uniprotTable_ProteinName <UID> <Uniprot Table ProteinName>
+	        //
+	        
+	        result = s.executeQuery("SELECT value " +
+	        		"FROM proteinnametype " +
+	        		"WHERE proteintype_name_hjid='" + id + "' " +
+	        		"AND proteintype_name_hjindex='0'");
+	        while(result.next()) {
+	        	uniprotTable_ProteinName.put(id, result.getString(1));
+	        	System.out.println("FOUND: " + result.getString(1));
+	        }
+        }
+        
+        result = s.executeQuery("SELECT value " +
+        		"FROM genenametype ");
+
+        
+        result = s.executeQuery("SELECT type, id " +
+		"FROM dbreferencetype ");
+
+        s.close();
+        
+	}
+	
+	public void PushToAccessDB() {
+		
+		try {
+			ExportToGenMaPP export = new ExportToGenMaPP(
+					System.getProperty("user.dir")+ "/dbFiles/GeneDBTmpl.mdb", System.getProperty("user.dir")+ "/dbFiles/output.mdb");
+		
+			export.openConnection();
+			
+			export.updateInfoTable("Loyola Marymount University",
+					"20060406", "UniProt", "|Escherichia coli K12|",
+					"", "20060406", "");
+			
+			List<String> systemCodeList = new ArrayList<String>();
+			systemCodeList.add("S");
+			
+			export.updateSystemsTable(systemCodeList, "20060406");
+			
+			export.createUniProtTable();
+			
+			for(String id : uniprotTable_ID.keySet()) {
+				
+				export.fillUniProtTable(uniprotTable_ID.get(id), 
+						uniprotTable_EntryName.get(id), 
+						uniprotTable_GeneName.get(id),
+						uniprotTable_ProteinName.get(id),
+						"", "|Escherichia coli K12|", "20060406", "");
+				
+			}
+
+		
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+	
+	public static void main(String args[]) {
+		
+		
+
+		  try{
+				// Create a new configuration
+			    final Configuration cfg = new Configuration();
+			    
+			   // cfg.addDirectory(new File("hbm/org/uniprot/uniprot"));
+
+			    // Create the properties for Hibernate
+			    final Properties properties = new Properties();
+			    properties.load(new FileInputStream(System.getProperty(
+			    		"user.dir") + System.getProperty("file.separator") +
+			    		"src" + System.getProperty("file.separator") +
+			    		"edu" + System.getProperty("file.separator") +
+			    		"lmu" + System.getProperty("file.separator") + 
+			    		"xmlpipedb" + System.getProperty("file.separator") +
+			    		"gmbuilder" + System.getProperty("file.separator") + 
+			    		"resource" + System.getProperty("file.separator") + 
+			    		"properties" + System.getProperty("file.separator") + 
+			    		"hibernate.properties"));
+			    
+			    cfg.setProperties(properties);
+			    
+			    
+		    // This step will read hibernate.cfg.xml and prepare hibernate for use
+			 SessionFactory sessionFactory = cfg.buildSessionFactory();
+			 Session session = sessionFactory.openSession();
+			 Transaction tx = session.beginTransaction();
+		     
+		      //Using from Clause
+		      String SQL_QUERY ="from Insurance insurance";	      
+		      Query query = session.createQuery(SQL_QUERY);
+		      
+		       for(Iterator it=query.iterate();it.hasNext();){
+		         Object o = it.next();
+		         System.out.println("ID: " + o.toString());
+		         System.out.println("First Name: " + o.toString());
+		       }
+		       
+		       tx.commit();
+		       session.close();
+		  }catch(Exception e){
+		    System.out.println(e.getMessage());
+		  }finally{
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		ExtractFromDB extract = new ExtractFromDB();
+//		System.out.println("CREATED POSTGRESQL CONNECTION...");
+//		try {
+//			extract.ExtractUniprotTableData();
+//			System.out.println("EXTRACTED DATA...");
+//			extract.PushToAccessDB();
+//			System.out.println("SAVED ACCESS DATABASE");
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		
+		
+		
+	}
+	
+	
+}

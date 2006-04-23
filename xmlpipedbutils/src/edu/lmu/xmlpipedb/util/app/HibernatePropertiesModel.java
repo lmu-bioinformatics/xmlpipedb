eae9e297e032b6da9e1dd146500d0b121732094e
dondi <dondi@lmu.edu>
2006-04-23 08:12:20 +0000

    Eliminated some easy-to-address warnings.  We like to be warning-free  :)

--- xmlpipedbutils/src/edu/lmu/xmlpipedb/util/app/HibernatePropertiesModel.java
+++ xmlpipedbutils/src/edu/lmu/xmlpipedb/util/app/HibernatePropertiesModel.java
@@ -1,7 +1,6 @@
 package edu.lmu.xmlpipedb.util.app;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -10,188 +9,183 @@ import java.util.Set;
 import java.util.Vector;
 
 public class HibernatePropertiesModel {
+    /**
+     * Returns all the categories available.
+     * 
+     * @return
+     */
+    public Set getCategories() {
+        HashMap<String, String> cats = new HashMap<String, String>();
+        Set keys = _properties.keySet();
+        Iterator iter = keys.iterator();
+        while (iter.hasNext()) {
+            String key = (String)iter.next();
+            HibernateProperty hp = _properties.get(key);
+            cats.put(hp.getCategory(), "");
+        }
 
-	
-	/**
-	 * Returns all the categories available.
-	 * 
-	 * @return
-	 */
-	public Set getCategories(){
-		HashMap cats = new HashMap();
-		Set keys = _properties.keySet();
-		Iterator iter = keys.iterator();
-		while( iter.hasNext() ){
-			String key = (String) iter.next();
-			HibernateProperty hp = (HibernateProperty)_properties.get(key);
-			cats.put(hp.getCategory(), "");
-		}
-		
-		return cats.keySet();
-	}
-	
-	/**
-	 * Returns a string array of type names for the category passed in.
-	 * The returned array is sorted alphabetically.
-	 * 
-	 * @param category
-	 * @return
-	 */
-	@SuppressWarnings("unchecked")
-	public String[] getTypes(String category){
-		HashMap types = new HashMap();
-		Set keys = _properties.keySet();
-		Iterator iter = keys.iterator();
-		while( iter.hasNext() ){
-			String key = (String) iter.next();
-			HibernateProperty hp = (HibernateProperty)_properties.get(key);
-			if( hp.getCategory().equals(category))
-				types.put(hp.getType(), hp.getType());
-		}
-		
-		// sort the list
-		ArrayList l = new ArrayList();
-		l.addAll(types.values());
-		Collections.sort(l);
-		
-		String[] ret = new String[l.size()];
-		l.toArray(ret);
-		
-		return ret;
-	}
-	
+        return cats.keySet();
+    }
 
-	/**
-	 * This method determines what type is saved as the selected type for the
-	 * category given.
-	 * 
-	 * @param category
-	 * @return String - the type selected for this category, if there is one otherwise null.
-	 */
-	public String getSelectedType(String category) {
-		String selectedType = null;
-		Set keys = _properties.keySet();
-		Iterator iter = keys.iterator();
-		while( iter.hasNext() ){
-			String key = (String) iter.next();
-			HibernateProperty hp = (HibernateProperty)_properties.get(key);
-			if( hp.getCategory().equals(category) && hp.isSaved() ){
-				selectedType = hp.getType();
-				break;
-			}
-		}
-		
-		return selectedType;
-	}
-	
-	/**
-	 * @param category
-	 * @param type
-	 * @return
-	 */
-	public Enumeration getPropertyNames(String category, String type){
-		Vector props = new Vector();
-		Set keys = _properties.keySet();
-		Iterator iter = keys.iterator();
-		while( iter.hasNext() ){
-			String key = (String) iter.next();
-			HibernateProperty hp = (HibernateProperty)_properties.get(key);
-			if( hp.getCategory().equals(category) && hp.getType().equals(type))
-				props.add(hp.getName());
-		}
-		
-		return props.elements();
-	}
-	
-	
-	/**
-	 * Returns an Enumeration with all the property names in the model
-	 * @return
-	 */
-	public Iterator getProperties(){
-		Set keys = _properties.keySet();
-		return keys.iterator();
-	}
-	
-	public ArrayList getProperties(String category, String type){
-		ArrayList props = new ArrayList();
-		Set keys = _properties.keySet();
-		Iterator iter = keys.iterator();
-		while( iter.hasNext() ){
-			String key = (String) iter.next();
-			HibernateProperty hp = (HibernateProperty)_properties.get(key);
-			if( hp.getCategory().equals(category) && hp.getType().equals(type))
-				props.add(hp);
-		}
-		
-		//return props.elements();
-		return props;
-	}
-	
-//	/**
-//	 * Returns an Enumeration with all the property names in the model
-//	 * @return
-//	 */
-//	public Iterator getProperties(){
-//		Set keys = _properties.keySet();
-//		return keys.iterator();
-//
-//	}
-	
-	/**
-	 * @param hp
-	 */
-	public void add( HibernateProperty hp ){
-		_properties.put(hp.getFullyQualifiedName(), hp);
-	}
-	
-	/**
-	 * Gets the HibernateProperty object for the given property name
-	 * @param propName
-	 * @return
-	 */
-	public HibernateProperty getProperty( String propName ){
-		return (HibernateProperty)_properties.get(propName);
-	}
-	
-	public void remove( HibernateProperty hp ){
-		_properties.remove(hp.getFullyQualifiedName());
-	}
-	
-	/**
-	 * @return Returns the currentCategory.
-	 */
-	public String getCurrentCategory() {
-		return currentCategory;
-	}
+    /**
+     * Returns a string array of type names for the category passed in. The
+     * returned array is sorted alphabetically.
+     * 
+     * @param category
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public String[] getTypes(String category) {
+        HashMap types = new HashMap();
+        Set keys = _properties.keySet();
+        Iterator iter = keys.iterator();
+        while (iter.hasNext()) {
+            String key = (String)iter.next();
+            HibernateProperty hp = _properties.get(key);
+            if (hp.getCategory().equals(category))
+                types.put(hp.getType(), hp.getType());
+        }
 
-	/**
-	 * @param currentCategory The currentCategory to set.
-	 */
-	public void setCurrentCategory(String currentCategory) {
-		this.currentCategory = currentCategory;
-	}
+        // sort the list
+        ArrayList l = new ArrayList();
+        l.addAll(types.values());
+        Collections.sort(l);
 
-	/**
-	 * @return Returns the currentType.
-	 */
-	public String getCurrentType() {
-		return currentType;
-	}
+        String[] ret = new String[l.size()];
+        l.toArray(ret);
 
-	/**
-	 * @param currentType The currentType to set.
-	 */
-	public void setCurrentType(String currentType) {
-		this.currentType = currentType;
-	}
-	
-	
-	//### DEFINE VARS ###
-	private HashMap _properties = new HashMap();
-	private String currentType = null;
-	private String currentCategory = null;
+        return ret;
+    }
 
+    /**
+     * This method determines what type is saved as the selected type for the
+     * category given.
+     * 
+     * @param category
+     * @return String - the type selected for this category, if there is one
+     *         otherwise null.
+     */
+    public String getSelectedType(String category) {
+        String selectedType = null;
+        Set keys = _properties.keySet();
+        Iterator iter = keys.iterator();
+        while (iter.hasNext()) {
+            String key = (String)iter.next();
+            HibernateProperty hp = _properties.get(key);
+            if (hp.getCategory().equals(category) && hp.isSaved()) {
+                selectedType = hp.getType();
+                break;
+            }
+        }
 
+        return selectedType;
+    }
 
+    /**
+     * @param category
+     * @param type
+     * @return
+     */
+    public Enumeration getPropertyNames(String category, String type) {
+        Vector<String> props = new Vector<String>();
+        Set keys = _properties.keySet();
+        Iterator iter = keys.iterator();
+        while (iter.hasNext()) {
+            String key = (String)iter.next();
+            HibernateProperty hp = _properties.get(key);
+            if (hp.getCategory().equals(category) && hp.getType().equals(type))
+                props.add(hp.getName());
+        }
+
+        return props.elements();
+    }
+
+    /**
+     * Returns an Enumeration with all the property names in the model
+     * 
+     * @return
+     */
+    public Iterator getProperties() {
+        Set keys = _properties.keySet();
+        return keys.iterator();
+    }
+
+    public ArrayList getProperties(String category, String type) {
+        ArrayList<HibernateProperty> props = new ArrayList<HibernateProperty>();
+        Set keys = _properties.keySet();
+        Iterator iter = keys.iterator();
+        while (iter.hasNext()) {
+            String key = (String)iter.next();
+            HibernateProperty hp = _properties.get(key);
+            if (hp.getCategory().equals(category) && hp.getType().equals(type))
+                props.add(hp);
+        }
+
+        // return props.elements();
+        return props;
+    }
+
+    // /**
+    // * Returns an Enumeration with all the property names in the model
+    // * @return
+    // */
+    // public Iterator getProperties(){
+    // Set keys = _properties.keySet();
+    // return keys.iterator();
+    //
+    // }
+
+    /**
+     * @param hp
+     */
+    public void add(HibernateProperty hp) {
+        _properties.put(hp.getFullyQualifiedName(), hp);
+    }
+
+    /**
+     * Gets the HibernateProperty object for the given property name
+     * 
+     * @param propName
+     * @return
+     */
+    public HibernateProperty getProperty(String propName) {
+        return (HibernateProperty)_properties.get(propName);
+    }
+
+    public void remove(HibernateProperty hp) {
+        _properties.remove(hp.getFullyQualifiedName());
+    }
+
+    /**
+     * @return Returns the currentCategory.
+     */
+    public String getCurrentCategory() {
+        return currentCategory;
+    }
+
+    /**
+     * @param currentCategory The currentCategory to set.
+     */
+    public void setCurrentCategory(String currentCategory) {
+        this.currentCategory = currentCategory;
+    }
+
+    /**
+     * @return Returns the currentType.
+     */
+    public String getCurrentType() {
+        return currentType;
+    }
+
+    /**
+     * @param currentType The currentType to set.
+     */
+    public void setCurrentType(String currentType) {
+        this.currentType = currentType;
+    }
+
+    //### DEFINE VARS ###
+    private HashMap<String, HibernateProperty> _properties = new HashMap<String, HibernateProperty>();
+    private String currentType = null;
+    private String currentCategory = null;
 }
--- xmlpipedbutils/src/edu/lmu/xmlpipedb/util/app/HibernateUtil.java
+++ xmlpipedbutils/src/edu/lmu/xmlpipedb/util/app/HibernateUtil.java
@@ -1,59 +1,57 @@
 package edu.lmu.xmlpipedb.util.app;
 
-//import org.logicalcobwebs.logging.Log;
-//import org.logicalcobwebs.logging.LogFactory;
-
 import java.util.Iterator;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.dialect.Dialect;
 
 import edu.lmu.xmlpipedb.util.resources.AppResources;
 import edu.lmu.xmlpipedb.util.utilities.ImportEngine;
+
+
 /**
- * This class maintains a set of hibernate utilities. The initial class and the closeSession() and currentSession() methods were downloaded from <a href="http://www.hibernate.org/hib_docs/reference/en/html_single/">http://www.hibernate.org/hib_docs/reference/en/html_single/</a>
+ * This class maintains a set of hibernate utilities. The initial class and the
+ * closeSession() and currentSession() methods were downloaded from <a
+ * href="http://www.hibernate.org/hib_docs/reference/en/html_single/">http://www.hibernate.org/hib_docs/reference/en/html_single/</a>
+ * 
  * @author Babak Naffas
- *
+ * 
  */
 public class HibernateUtil {
 
-    //private static Log log = LogFactory.getLog(HibernateUtil.class);
+    // private static Log log = LogFactory.getLog(HibernateUtil.class);
 
     private static final SessionFactory sessionFactory;
 
     static {
         try {
-     	
-        	//Since the ImportEngine is working fine, let's use it's hibernate settings for now.
-        	String context = AppResources.optionString("jaxbContextPath");
-			String hibernateProp = AppResources.optionString("hibernateProperties");
-	
-			String hibernateConfig = AppResources
-					.optionString("hibernateMappingDir");
-			ImportEngine ie = new ImportEngine(context, hibernateConfig,
-					hibernateProp);
-			
-			sessionFactory = ie.getSessionFactory();        	
-        	
-        }
-        catch (Throwable ex) {
-        	System.err.println( "Initial SessionFactory creation failed.\n" + ex );
+            // Since the ImportEngine is working fine, let's use its hibernate
+            // settings for now.
+            String context = AppResources.optionString("jaxbContextPath");
+            String hibernateProp = AppResources.optionString("hibernateProperties");
+
+            String hibernateConfig = AppResources.optionString("hibernateMappingDir");
+            ImportEngine ie = new ImportEngine(context, hibernateConfig, hibernateProp);
+
+            sessionFactory = ie.getSessionFactory();
+        } catch(Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed.\n" + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
 
-    public static final ThreadLocal session = new ThreadLocal();
+    private static final ThreadLocal<Session> session = new ThreadLocal<Session>();
 
     /**
      * Returns an instance of the current Hibernate Session.
+     * 
      * @return Session reference.
      * @throws HibernateException
      */
     public static Session currentSession() throws HibernateException {
-        Session s = (Session) session.get();
+        Session s = (Session)session.get();
         // Open a new Session, if this Thread has none yet
         if (s == null) {
             s = sessionFactory.openSession();
@@ -63,52 +61,54 @@ public class HibernateUtil {
     }
 
     /**
-     * Closses the hibernate session. Modified by Babak Naffas to catch HibernateException within the method and handle it here. 
+     * Closses the hibernate session. Modified by Babak Naffas to catch
+     * HibernateException within the method and handle it here.
      * 
      * @version 1.0 Originally downloaded implementation
-     * @version 1.1 Modified by Babak Naffas to catch HibernateException within the method and handle it here.
+     * @version 1.1 Modified by Babak Naffas to catch HibernateException within
+     *          the method and handle it here.
      */
     public static void closeSession() {
-    	try{
-    		Session s = (Session) session.get();
-    		session.set(null);
-    		if (s != null)
-    			s.close();
-    	}
-    	catch( HibernateException he ){
-    		System.err.println( he.getMessage() );
-    	}
+        try {
+            Session s = (Session)session.get();
+            session.set(null);
+            if (s != null)
+                s.close();
+        } catch(HibernateException he) {
+            System.err.println(he.getMessage());
+        }
     }
-    
+
     /**
      * Executes an HQL (Hibernate Wuery Language) query.
-     * @param hql	The query to execute.
-     * @return	The resulting java.util.Iterator referencing the results of the query.  
+     * 
+     * @param hql
+     *            The query to execute.
+     * @return The resulting java.util.Iterator referencing the results of the
+     *         query.
      */
-    public static Iterator executeHQL( String hql ){
-    	
-    	Session session = null;
-    	Query query = null;
-    	Iterator iter = null;
-    	
-    	try{
-    		session = currentSession();
-    		query = session.createQuery( hql );
-    		iter = query.iterate();
-    	}
-    	catch( HibernateException he ){
-    		System.err.println( he.getMessage() );
-    	}
-		catch( Exception e){
-			System.err.println( e.getMessage() );
-			
-		}
-    	
-    	//closeSession();
-    	return iter;
+    public static Iterator executeHQL(String hql) {
+
+        Session session = null;
+        Query query = null;
+        Iterator iter = null;
+
+        try {
+            session = currentSession();
+            query = session.createQuery(hql);
+            iter = query.iterate();
+        } catch(HibernateException he) {
+            System.err.println(he.getMessage());
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+
+        }
+
+        // closeSession();
+        return iter;
     }
-    
-    /*public Dialect getDialect(){
-    	sessionFactory.
-    }*/
+
+    /*
+     * public Dialect getDialect(){ sessionFactory. }
+     */
 }

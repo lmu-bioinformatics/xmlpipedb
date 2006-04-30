81895688c89ee2b8b1a3bc337de43962413330ff
holiscan <holiscan@1094f71c-c033-0410-a037-bb4d46bb0a42>
2006-04-30 05:45:59 +0000

    *** empty log message ***

--- trunk/xmlpipedbutils/src/edu/lmu/xmlpipedb/util/exceptions/CouldNotLoadPropertiesException.java
+++ trunk/xmlpipedbutils/src/edu/lmu/xmlpipedb/util/exceptions/CouldNotLoadPropertiesException.java
@@ -2,29 +2,26 @@ package edu.lmu.xmlpipedb.util.exceptions;
 
 public class CouldNotLoadPropertiesException extends Exception {
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Creates CouldNotLoadPropertiesException exception with no
+	 * explanatory text.
+	 */
 	public CouldNotLoadPropertiesException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Creates CouldNotLoadPropertiesException exception with a message
+	 * explaining what went wrong.
+	 * 
+	 * @param arg0
+	 */
 	public CouldNotLoadPropertiesException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
-	public CouldNotLoadPropertiesException(String arg0, Throwable arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
-
-	public CouldNotLoadPropertiesException(Throwable arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
 
 }
--- trunk/xmlpipedbutils/src/edu/lmu/xmlpipedb/util/exceptions/NoHibernatePropertiesException.java
+++ trunk/xmlpipedbutils/src/edu/lmu/xmlpipedb/util/exceptions/NoHibernatePropertiesException.java
@@ -2,29 +2,25 @@ package edu.lmu.xmlpipedb.util.exceptions;
 
 public class NoHibernatePropertiesException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
+	
+	/**
+	 * Creates NoHibernatePropertiesException with no explanatory text.
+	 */
 	public NoHibernatePropertiesException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Creates NoHibernatePropertiesException with a message to explain
+	 * what went wrong.
+	 * 
+	 * @param arg0
+	 */
 	public NoHibernatePropertiesException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
-	public NoHibernatePropertiesException(String arg0, Throwable arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
-
-	public NoHibernatePropertiesException(Throwable arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
 
 }

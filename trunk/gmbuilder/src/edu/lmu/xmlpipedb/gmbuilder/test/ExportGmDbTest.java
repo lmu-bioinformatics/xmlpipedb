4224c1b105384ef9c53acf649883a45e82be0fda
joeyjbarrett <joeyjbarrett@1094f71c-c033-0410-a037-bb4d46bb0a42>
2006-06-22 21:39:26 +0000

    Commented out code for use with old export. --Joey J. Barrett

--- trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/test/ExportGmDbTest.java
+++ trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/test/ExportGmDbTest.java
@@ -1,24 +1,15 @@
 package edu.lmu.xmlpipedb.gmbuilder.test;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.xml.bind.JAXBException;
-
 import junit.framework.Assert;
 import junit.framework.TestCase;
-
-import org.hibernate.HibernateException;
-import org.xml.sax.SAXException;
-
-import shag.dialog.ModalDialog;
-import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.ExportToGenMaPP;
+//import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.ExportToGenMaPP;
 
 public class ExportGmDbTest extends TestCase {
 
@@ -32,30 +23,30 @@ public class ExportGmDbTest extends TestCase {
 	
 	
 	protected void setUp() throws FileNotFoundException{
-		try {
-			ExportToGenMaPP.exportToGenMaPP(new File("./src/edu/lmu/xmlpipedb/gmbuilder/test/gmb_unit_test.mdb"));
-		} catch (HibernateException e) {
-			ModalDialog.showErrorDialog("HIBERNATE error.");
-			e.printStackTrace();
-		} catch (SAXException e) {
-			ModalDialog.showErrorDialog("SAX error.");
-			e.printStackTrace();
-		} catch (JAXBException e) {
-			ModalDialog.showErrorDialog("JAXB error.");
-			e.printStackTrace();
-		} catch (SQLException e) {			
-            ModalDialog.showErrorDialog("SQL error.");
-            e.printStackTrace();
-		} catch (IOException e) {
-			ModalDialog.showErrorDialog("I/O error.");
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			ModalDialog.showErrorDialog("Database driver error.");
-			e.printStackTrace();
-		} catch (Exception e) {
-			ModalDialog.showErrorDialog(e.toString());
-			e.printStackTrace();
-		}
+//		try {
+//			ExportToGenMaPP.exportToGenMaPP(new File("./src/edu/lmu/xmlpipedb/gmbuilder/test/gmb_unit_test.mdb"));
+//		} catch (HibernateException e) {
+//			ModalDialog.showErrorDialog("HIBERNATE error.");
+//			e.printStackTrace();
+//		} catch (SAXException e) {
+//			ModalDialog.showErrorDialog("SAX error.");
+//			e.printStackTrace();
+//		} catch (JAXBException e) {
+//			ModalDialog.showErrorDialog("JAXB error.");
+//			e.printStackTrace();
+//		} catch (SQLException e) {			
+//            ModalDialog.showErrorDialog("SQL error.");
+//            e.printStackTrace();
+//		} catch (IOException e) {
+//			ModalDialog.showErrorDialog("I/O error.");
+//			e.printStackTrace();
+//		} catch (ClassNotFoundException e) {
+//			ModalDialog.showErrorDialog("Database driver error.");
+//			e.printStackTrace();
+//		} catch (Exception e) {
+//			ModalDialog.showErrorDialog(e.toString());
+//			e.printStackTrace();
+//		}
 	}
 	
 

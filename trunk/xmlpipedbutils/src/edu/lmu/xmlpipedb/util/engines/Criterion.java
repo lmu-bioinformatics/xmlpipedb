8916c627510d37330f27ac80458c7eeb33263167
holiscan <holiscan@1094f71c-c033-0410-a037-bb4d46bb0a42>
2006-10-19 13:44:04 +0000

    Updated to provide database counts. The Criterion object had to change to have separate DB and XML file counts.

--- trunk/xmlpipedbutils/src/edu/lmu/xmlpipedb/util/engines/Criterion.java
+++ trunk/xmlpipedbutils/src/edu/lmu/xmlpipedb/util/engines/Criterion.java
@@ -45,24 +45,50 @@ public class Criterion {
 		this._table = table;
 	}
 	/**
-	 * @return Returns the count.
+	 * @return Returns the Database record count for the table specified. If
+	 * no table has been specified or this count has not been obtained, the
+	 * value will be -1.
 	 */
-	public int getCount() {
-		return _count;
+	public int getDbCount() {
+		return _dbCount;
 	}
 	/**
 	 * @param Count The count to set.
 	 */
-	public void setCount(int count) {
-		this._count = count;
+	public void setDbCount(int count) {
+		this._dbCount = count;
 	}
+	
+	/**
+	 * @return Returns the Database record count for the table specified. If
+	 * no table has been specified or this count has not been obtained, the
+	 * value will be -1.
+	 */
+	public int getXmlCount() {
+		return _xmlCount;
+	}
+	/**
+	 * Sets the count to the value passed in. The caller must be aware that
+	 * the initial count is set to -1, so if getXmlCount is being called and
+	 * incremented by 1 before calling setXmlCount, the count may be off by 1
+	 * unless the count was initialized to 0 by the caller before the series
+	 * was started.
+	 * 
+	 * @param Count The count to set.
+	 */
+	public void setXmlCount(int count) {
+		this._xmlCount = count;
+	}
+
+	
 	
 
 	// CLASS MEMBERS
 	private String _name;
 	private String _digesterPath;
 	private String _table;
-	private int _count;
+	private int _dbCount = -1;
+	private int _xmlCount = -1;
 	
 	
 }

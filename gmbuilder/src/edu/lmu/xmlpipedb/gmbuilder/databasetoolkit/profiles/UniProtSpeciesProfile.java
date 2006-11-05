6884accc300eb601f9c9bc780a87fcb9a9badea4
holiscan <holiscan@1094f71c-c033-0410-a037-bb4d46bb0a42>
2006-11-05 13:37:55 +0000

    Completed refactor. Some code left inline as comments, incase the decisions made were not valid.

--- gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/UniProtSpeciesProfile.java
+++ gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/UniProtSpeciesProfile.java
@@ -63,7 +63,10 @@ public class UniProtSpeciesProfile extends SpeciesProfile {
     public TableManager getSystemTableManagerCustomizations(TableManager tableManager, TableManager primarySystemTableManager, Date version) throws SQLException {
         // TODO This is virtually identical to the e. coli version; find a way to unify.
         // TODO (for that matter, find a way to unify the whole thing)
-        PreparedStatement ps = ConnectionManager.getRelationalDBConnection().prepareStatement("SELECT value, type " + "FROM genenametype INNER JOIN entrytype_genetype " + "ON (entrytype_genetype_name_hjid = entrytype_genetype.hjid) " + "WHERE entrytype_gene_hjid = ?");
+    	
+    	tableManager = systemTableManagerCustomizationsHelper(tableManager, primarySystemTableManager, version, "OrderedLocusNames");
+    	
+/*        PreparedStatement ps = ConnectionManager.getRelationalDBConnection().prepareStatement("SELECT value, type " + "FROM genenametype INNER JOIN entrytype_genetype " + "ON (entrytype_genetype_name_hjid = entrytype_genetype.hjid) " + "WHERE entrytype_gene_hjid = ?");
         ResultSet result;
 
         for (Row row : primarySystemTableManager.getRows()) {
@@ -80,11 +83,39 @@ public class UniProtSpeciesProfile extends SpeciesProfile {
                     }
                 }
             }
-        }
+        }*/
 
         return tableManager;
     }
 
+	/**
+     * @see edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.profiles.SpeciesProfile#getSystemTableManagerCustomizations(edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager,
+     *      edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager,
+     *      java.util.Date)
+     */
+    protected TableManager systemTableManagerCustomizationsHelper(TableManager tableManager, TableManager primarySystemTableManager, Date version, String substituteTable ) throws SQLException {
+        PreparedStatement ps = ConnectionManager.getRelationalDBConnection().prepareStatement("SELECT value, type " + "FROM genenametype INNER JOIN entrytype_genetype " + "ON (entrytype_genetype_name_hjid = entrytype_genetype.hjid) " + "WHERE entrytype_gene_hjid = ?");
+        ResultSet result;
+
+        for (Row row : primarySystemTableManager.getRows()) {
+            ps.setString(1, row.getValue("UID"));
+            result = ps.executeQuery();
+
+            // We actually want to keep the case where multiple ordered locus names appear.
+            while (result.next()) {
+                String type = result.getString("type");
+                if ("ordered locus".equals(type) || "ORF".equals(type)) {
+                    // We want this name to appear in the OrderedLocusNames system table.
+                    for (String id : result.getString("value").split("/")) {
+                        tableManager.submit(substituteTable, QueryType.insert, new String[][] { { "ID", id }, { "Species", "|" + getSpeciesName() + "|" }, { "\"Date\"", GenMAPPBuilderUtilities.getSystemsDateString(version) }, { "UID", row.getValue("UID") } });
+                    }
+                }
+            }
+        }
+
+        return tableManager;
+    }
+    
 	/* (non-Javadoc)
 	 * @see edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.profiles.SpeciesProfile#getRelationsTableManagerCustomizations(java.lang.String, java.lang.String, java.util.Map, edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager)
 	 */
@@ -113,16 +144,6 @@ public class UniProtSpeciesProfile extends SpeciesProfile {
 				}
 			);
 		
-			/*tableManager.submit("Relations", QueryType.insert, new String[][] {
-					//{"SystemCode", templateDefinedSystemToSystemCode.get(!systemTable1.equals("OrderedLocusNames") ? systemTable1 : "OrderedLocusNames")},
-					{"SystemCode", templateDefinedSystemToSystemCode.get(systemTable1)},
-					//{"RelatedCode", templateDefinedSystemToSystemCode.get(!systemTable2.equals("OrderedLocusNames") ? systemTable2 : "OrderedLocusNames")},
-					{"RelatedCode", templateDefinedSystemToSystemCode.get(systemTable2)},
-					{"Relation", systemTable1 + "-" + systemTable2},
-					{"Type", systemTable1.equals("UniProt") ||
-						systemTable2.equals("UniProt") ? "Direct" : "Inferred"},
-					{"Source", ""}
-			});*/
 		return tableManager;
 	}
 	
@@ -134,7 +155,10 @@ public class UniProtSpeciesProfile extends SpeciesProfile {
      */
     @Override
     public TableManager getSpeciesSpecificRelationshipTable(String relationshipTable, TableManager uniprotTableManager, TableManager systemTableManager, TableManager tableManager) throws SQLException {
-        SystemTablePair stp = GenMAPPBuilderUtilities.parseRelationshipTableName(relationshipTable);
+    	
+    	tableManager = speciesSpecificRelationshipTableHelper(relationshipTable, uniprotTableManager, systemTableManager, tableManager, "OrderedLocusNames", "Bridge", "S");
+    	
+/*        SystemTablePair stp = GenMAPPBuilderUtilities.parseRelationshipTableName(relationshipTable);
         if (getSpeciesSpecificSystemTables().containsKey(stp.systemTable1)) {
             PreparedStatement ps = ConnectionManager.getRelationalDBConnection().prepareStatement("SELECT id " + "FROM dbreferencetype " + "WHERE type = ? and " + "entrytype_dbreference_hjid = ?");
             ps.setString(1, stp.systemTable2);
@@ -183,11 +207,73 @@ public class UniProtSpeciesProfile extends SpeciesProfile {
                     }
                 }
             }
+        }*/
+
+        return tableManager;
+    }
+
+	/**
+     * @see edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.profiles.SpeciesProfile#getSpeciesSpecificRelationshipTable(java.lang.String,
+     *      edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager,
+     *      edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager,
+     *      edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager)
+     */
+    public TableManager speciesSpecificRelationshipTableHelper(String relationshipTable, TableManager uniprotTableManager, TableManager systemTableManager, TableManager tableManager, String criteria, String finalColumnName, String finalColumnValue) throws SQLException {
+        SystemTablePair stp = GenMAPPBuilderUtilities.parseRelationshipTableName(relationshipTable);
+        if (getSpeciesSpecificSystemTables().containsKey(stp.systemTable1)) {
+            PreparedStatement ps = ConnectionManager.getRelationalDBConnection().prepareStatement("SELECT id " + "FROM dbreferencetype " + "WHERE type = ? and " + "entrytype_dbreference_hjid = ?");
+            ps.setString(1, stp.systemTable2);
+            ResultSet result;
+            for (Row row : systemTableManager.getRows()) {
+                if (row.getValue(TableManager.TABLE_NAME_COLUMN).equals(criteria)) {
+                    ps.setString(2, row.getValue("UID"));
+                    result = ps.executeQuery();
+                    while (result.next()) {
+                        tableManager.submit(relationshipTable, QueryType.insert, new String[][] { { "\"Primary\"", row.getValue("ID") }, { "Related", result.getString("id") },
+                        // TODO This is hard-coded. Fix it.
+                        { finalColumnName, finalColumnValue } });
+                    }
+                }
+            }
+            ps.close();
+        } else if (getSpeciesSpecificSystemTables().containsKey(stp.systemTable2) && !stp.systemTable1.equals("UniProt")) {
+            PreparedStatement ps = ConnectionManager.getRelationalDBConnection().prepareStatement("SELECT entrytype_dbreference_hjid, id " + "FROM dbreferencetype where type = ?");
+            ps.setString(1, stp.systemTable1);
+            ResultSet result = ps.executeQuery();
+
+            while (result.next()) {
+                String primary = result.getString("id");
+                String related = result.getString("entrytype_dbreference_hjid");
+
+                for (Row row : systemTableManager.getRows()) {
+                    if (row.getValue(TableManager.TABLE_NAME_COLUMN).equals(criteria) && row.getValue("UID").equals(related)) {
+                        for (String id : row.getValue("ID").split("/")) {
+                            tableManager.submit(relationshipTable, QueryType.insert, new String[][] { { "\"Primary\"", primary }, { "Related", id },
+                            // TODO This is hard-coded. Fix it.
+                            { finalColumnName, finalColumnValue } });
+                        }
+                    }
+                }
+            }
+            ps.close();
+        } else {
+            for (Row row1 : systemTableManager.getRows()) {
+                if (row1.getValue(TableManager.TABLE_NAME_COLUMN).equals(criteria)) {
+                    for (Row row2 : uniprotTableManager.getRows()) {
+                        if (row2.getValue("UID").equals(row1.getValue("UID"))) {
+                            tableManager.submit(relationshipTable, QueryType.insert, new String[][] { { "\"Primary\"", row2.getValue("ID") }, { "Related", row1.getValue("ID") },
+                            //TODO This is hard-coded.  Fix it. 
+                            { finalColumnName, finalColumnValue } });
+                        }
+                    }
+                }
+            }
         }
 
         return tableManager;
     }
 
+    
 	/* (non-Javadoc)
 	 * @see edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.profiles.SpeciesProfile#getSpeciesSpecificSystemCode(java.util.List, java.util.Map)
 	 */

108b8ccf6b038e821af739ef8e9d55dd90aaa48a
dondi <dondi@lmu.edu>
2007-07-13 22:25:13 +0000

    Separated TAIR ID collection routine, plus a unit test wrapper so it can be called on its own.

--- gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/ArabidopsisThalianaUniProtSpeciesProfile.java
+++ gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/ArabidopsisThalianaUniProtSpeciesProfile.java
@@ -201,146 +201,36 @@ public class ArabidopsisThalianaUniProtSpeciesProfile extends UniProtSpeciesProf
 	public TableManager getSystemTableManagerCustomizations(
 			TableManager tableManager, TableManager primarySystemTableManager,
 			Date version) {
-	    // All TAIR IDs follow this pattern.
-	    final String tairID = "[Aa][Tt][0-9][CcMmGg][0-9][0-9][0-9][0-9][0-9]";
+	    // Gather up the TAIR IDs into a temporary table.
+        (new ArabidopsisTAIRIDCollector()).collectTAIRIDs(ConnectionManager.getRelationalDBConnection());
 
         PreparedStatement ps;
         ResultSet result;
         String dateToday = GenMAPPBuilderUtilities.getSystemsDateString(version);
-        int splits = -1;
         String sqlQuery;
-        
-		//step 1 - put the dbreference values for TAIR into a temp table, at the
-        // same time "cleaning out" any prefixes and suffixes (via the substring
-        // function)
-		sqlQuery = 
-		"create temporary table temp_tair as " +
-		"select a.entrytype_dbreference_hjid as hjid , substring(a.id from ?) from dbreferencetype a " +
-		"a.id SIMILAR TO ? " +
-		"group by a.id, a.entrytype_dbreference_hjid";
-        try {
-	    	ps = ConnectionManager.getRelationalDBConnection().prepareStatement(sqlQuery);
-            ps.setString(1, tairID);
-	    	ps.setString(2, "%" + tairID + "%");
-	    	ps.executeUpdate();
-		} catch (SQLException e) {
-            logSQLException(e, sqlQuery);
-		}
-		
-		// step 1a - TAIR IDs can also be found in propertytype
-		sqlQuery = "insert into temp_tair " +
-		    "select d.entrytype_dbreference_hjid as hjid, p.value " +
-		    "from propertytype p inner join dbreferencetype d on (p.dbreferencetype_property_hjid = d.hjid) " +
-		    "where p.value similar to ? " +
-		    "group by d.entrytype_dbreference_hjid, p.value";
+
+        // step 5 - extract the complete list of TAIR IDs into a resultset
+        sqlQuery = "select id from temp_tair group by id";
         try {
             ps = ConnectionManager.getRelationalDBConnection().prepareStatement(sqlQuery);
-            // We don't accommodate prefixes nor suffixes in propertytype.
-            ps.setString(1, tairID);
-            ps.executeUpdate();
-        } catch (SQLException e) {
+            result = ps.executeQuery();
+            int i = 0;
+            // step 5 - put the tair ids into the tablemanager
+            for (; result.next(); i++) {
+                // tableManager.submit(substituteTable, QueryType.insert, new
+                // String[][] { { "ID", id }, { "Species", "|" +
+                // getSpeciesName() + "|" }, { "\"Date\"", dateToday }, { "UID",
+                // row.getValue("UID") } });
+                _Log.debug("getSystemTableManagerCustomizations(): TAIR ID" + result.getString("id"));
+                _Log.debug("Row #: [" + i + "]");
+                tableManager.submit("TAIR", QueryType.insert, new String[][] { { "ID", result.getString("id") }, { "Species", "|" + getSpeciesName() + "|" }, { "\"Date\"", dateToday } });
+            }
+            _Log.info("TAIR Records Written to TableManger: [" + i + "]");
+        } catch(SQLException e) {
             logSQLException(e, sqlQuery);
         }
 
-		//step 2 - put the genename values that match the TAIR pattern exactly* into the temp table
-		//* some genename records have more than one TAIR ID per cell and they 
-		//   must have their values split out in step 3 below.
-		sqlQuery =
-        "insert into temp_tair " +
-        "select d.entrytype_gene_hjid as hjid, c.value " +
-        "from genenametype c INNER JOIN entrytype_genetype d ON (c.entrytype_genetype_name_hjid = d.hjid) " +
-        "where c.value SIMILAR TO ? " +
-        "group by d.entrytype_gene_hjid, c.value";
-        try {
-	    	ps = ConnectionManager.getRelationalDBConnection().prepareStatement(sqlQuery);
-	    	ps.setString(1, tairID);
-	        ps.executeUpdate();
-		} catch (SQLException e) {
-            logSQLException(e, sqlQuery);
-		}
-			
-		//step 3 - get the genename values for the records with multiple TAIR IDs
-		
-		/* JD: I see what Jeffrey is trying to do here, but the logic breaks
-		 * somewhat when we consider that:
-		 * 
-		 * (a) the "separator" isn't just a slash; it may also be a colon,
-		 *     a semicolon, or a space
-		 *
-		 * (b) many composite IDs consist of more than just TAIR IDs; i.e., they
-		 *     may use other formats that aren't necessarily 9 characters long
-		 * 
-		 * (c) see below for the proposed alternative
-		 */
-		// 3a. - determine the largest number of values in one cell
-		sqlQuery =
-    	" select max(char_length(c.value)) as maxLength " +
-		" from genenametype c " + 
-		" where c.value SIMILAR TO ?";
-        try {
-        	ps = ConnectionManager.getRelationalDBConnection().prepareStatement(sqlQuery);
-        	ps.setString(1, "%/" + tairID + "/*A%");
-	        result = ps.executeQuery();
-			// get result MOD 9 (number of characters in ID) = number of "/" characters in longest value
-	        result.next();
-	        int maxLength = result.getInt("maxLength");
-	        splits = maxLength / 9; // 9 is the length of the TAIR ID (At1g12345)
-		} catch (SQLException e) {
-            logSQLException(e, sqlQuery);
-		}
-		
-			//step 4 - generate the query that will put values from each column of splits into the temp table
-	        for( int i = 1; i <= splits; i++){
-	            try {
-	            	sqlQuery = 
-	            	"insert into temp_tair " +
-	            	"select hjid, value from ( " +
-	            	"select d.entrytype_gene_hjid as hjid, split_part(c.value, '/', " + i + ") as value " +
-	            	"from genenametype c INNER JOIN entrytype_genetype d ON (c.entrytype_genetype_name_hjid = d.hjid) " +
-	            	"where c.value SIMILAR TO ?) as split_gene " +
-	            	"where value SIMILAR TO ? ";
-	                ps = ConnectionManager.getRelationalDBConnection().prepareStatement(sqlQuery);
-	                ps.setString(1, "%" + tairID + "/%");
-	                ps.setString(2, tairID);
-	                ps.executeUpdate();
-	            } catch (SQLException e) {
-	                logSQLException(e, sqlQuery);
-	            }
-	        }
-
-		// Step 3: extract the genenametype IDs that are likely to be lists of
-		// further IDs, with at least one TAIR ID among them.  For each of these
-		// potential lists, extract the TAIR IDs, then insert a new individual
-		// record for each of them.
-		//
-		// FIXME Yes, this is nasty slow.  But more accurate and general.
-//        sqlQuery =
-//            "select d.entrytype_gene_hjid as hjid, c.value " +
-//            "from genenametype c INNER JOIN entrytype_genetype d ON (c.entrytype_genetype_name_hjid = d.hjid) " +
-//            "where c.value SIMILAR TO ? and char_length(c.value) > ? " +
-//            "group by d.entrytype_gene_hjid, c.value";
-
-			//step 5 - extract the complete list of TAIR IDs into a resultset
-	        sqlQuery = 
-	        " select id from temp_tair where id SIMILAR TO ? group by id"; 
-	        try {
-			    ps = ConnectionManager.getRelationalDBConnection().prepareStatement(sqlQuery);
-			    ps.setString(1, tairID);
-		        result = ps.executeQuery();
-		        int i = 0;
-				//step 5 - put the tair ids into the tablemanager
-		        for(; result.next(); i++) {
-		            //tableManager.submit(substituteTable, QueryType.insert, new String[][] { { "ID", id }, { "Species", "|" + getSpeciesName() + "|" }, { "\"Date\"", dateToday }, { "UID", row.getValue("UID") } });
-		        	_Log.debug("getSystemTableManagerCustomizations(): TAIR ID" + result.getString("id") );
-		            _Log.debug("Row #: [" + i + "]");
-		            tableManager.submit("TAIR", QueryType.insert, new String[][] { { "ID", result.getString("id") }, { "Species", "|" + getSpeciesName() + "|" }, { "\"Date\"", dateToday } });
-		        }
-		        _Log.info("TAIR Records Written to TableManger: [" + i + "]");
-			} catch (SQLException e) {
-			    logSQLException(e, sqlQuery);
-			}
-
-		return tableManager;
+        return tableManager;
 	}
 
 	/**

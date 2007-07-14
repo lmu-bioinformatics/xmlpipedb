5aa7aaff9e23c422d78a25dc04068abd3ba3663a
zugzugglug <zugzugglug@1094f71c-c033-0410-a037-bb4d46bb0a42>
2007-07-14 07:08:38 +0000

    New implementation for splitting IDs; previous SQL-only implementation missed some cases.  This new approach shifts the heavy lifting to the CPU, but is more general and captures more TAIR IDs.

--- trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/ArabidopsisTAIRIDCollector.java
+++ trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/ArabidopsisTAIRIDCollector.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -12,6 +16,10 @@ import org.apache.commons.logging.LogFactory;
  * The ArabidopsisTAIRIDCollector "harvests" TAIR IDs from a UniProt Arabidopsis
  * database. The utility is separated for execution outside of GenMAPP Builder
  * as well as easier unit testing.
+ * 
+ * Implementation note: This routine assumes support for "similar to" regexes in
+ * addition to the substring(source from regex) and upper(string) functions.
+ * PostgreSQL has them, but other databases may not.
  * 
  * @author dondi
  */
@@ -33,32 +41,46 @@ public class ArabidopsisTAIRIDCollector {
         // All TAIR IDs follow this pattern.
         final String tairID = "[Aa][Tt][0-9][CcMmGg][0-9][0-9][0-9][0-9][0-9]";
 
+        // Wrap this all in a transaction.
+        boolean priorAutoCommit = true;
+        try {
+            priorAutoCommit = c.getAutoCommit();
+            c.setAutoCommit(false);
+        } catch(SQLException e) {
+            logSQLException(e, "Setting autocommit to false");
+        }
+
         PreparedStatement ps;
         ResultSet result;
-        int splits = -1;
         String sqlQuery;
-        
-        //step 1 - put the dbreference values for TAIR into a temp table, at the
-        // same time "cleaning out" any prefixes and suffixes (via the substring
-        // function)
-        sqlQuery = "create table " + (temporary ? "temporary" : "") +
+
+        // Step 1: Put the dbreference values for TAIR into a temp table, at
+        // the same time "cleaning out" any prefixes and suffixes (via the
+        // substring function)
+        _Log.info("Collecting TAIR IDs from dbreference...");
+        sqlQuery = "create table " +
+            (temporary ? "temporary" : "") +
             " temp_tair as " +
-            "select a.entrytype_dbreference_hjid as hjid, substring(a.id from ?) as id " +
+            "select a.entrytype_dbreference_hjid as hjid, " +
+            "substring(a.id from ?) as id " +
             "from dbreferencetype a where a.id similar to ? " +
             "group by a.id, a.entrytype_dbreference_hjid";
         try {
             ps = c.prepareStatement(sqlQuery);
             ps.setString(1, tairID);
+            // TAIR IDs in dbreference may have prefixes and postfixes.
             ps.setString(2, "%" + tairID + "%");
             ps.executeUpdate();
         } catch(SQLException e) {
             logSQLException(e, sqlQuery);
         }
-        
-        // step 1a - TAIR IDs can also be found in propertytype
+
+        // Step 2: TAIR IDs can also be found in propertytype
+        _Log.info("Collecting TAIR IDs from propertytype...");
         sqlQuery = "insert into temp_tair " +
             "select d.entrytype_dbreference_hjid as hjid, p.value " +
-            "from propertytype p inner join dbreferencetype d on (p.dbreferencetype_property_hjid = d.hjid) " +
+            "from propertytype p inner join dbreferencetype d " +
+            "on (p.dbreferencetype_property_hjid = d.hjid) " +
             "where p.value similar to ? " +
             "group by d.entrytype_dbreference_hjid, p.value";
         try {
@@ -66,95 +88,87 @@ public class ArabidopsisTAIRIDCollector {
             // We don't accommodate prefixes nor suffixes in propertytype.
             ps.setString(1, tairID);
             ps.executeUpdate();
-        } catch (SQLException e) {
+        } catch(SQLException e) {
             logSQLException(e, sqlQuery);
         }
 
-        //step 2 - put the genename values that match the TAIR pattern exactly* into the temp table
-        //* some genename records have more than one TAIR ID per cell and they 
-        //   must have their values split out in step 3 below.
-        sqlQuery =
-        "insert into temp_tair " +
-        "select d.entrytype_gene_hjid as hjid, c.value " +
-        "from genenametype c INNER JOIN entrytype_genetype d ON (c.entrytype_genetype_name_hjid = d.hjid) " +
-        "where c.value SIMILAR TO ? " +
-        "group by d.entrytype_gene_hjid, c.value";
-        try {
-            ps = c.prepareStatement(sqlQuery);
-            ps.setString(1, tairID);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            logSQLException(e, sqlQuery);
-        }
-            
-        //step 3 - get the genename values for the records with multiple TAIR IDs
-        
-        /* JD: I see what Jeffrey is trying to do here, but the logic breaks
-         * somewhat when we consider that:
-         * 
-         * (a) the "separator" isn't just a slash; it may also be a colon,
-         *     a semicolon, or a space
-         *
-         * (b) many composite IDs consist of more than just TAIR IDs; i.e., they
-         *     may use other formats that aren't necessarily 9 characters long
-         * 
-         * (c) see below for the proposed alternative
-         */
-        // 3a. - determine the largest number of values in one cell
-        sqlQuery =
-        " select max(char_length(c.value)) as maxLength " +
-        " from genenametype c " + 
-        " where c.value SIMILAR TO ?";
-        try {
-            ps = c.prepareStatement(sqlQuery);
-            ps.setString(1, "%/" + tairID + "/*A%");
-            result = ps.executeQuery();
-            // get result MOD 9 (number of characters in ID) = number of "/" characters in longest value
-            result.next();
-            int maxLength = result.getInt("maxLength");
-            splits = maxLength / 9; // 9 is the length of the TAIR ID (At1g12345)
-        } catch (SQLException e) {
-            logSQLException(e, sqlQuery);
-        }
-        
-            //step 4 - generate the query that will put values from each column of splits into the temp table
-            for( int i = 1; i <= splits; i++){
-                try {
-                    sqlQuery = 
-                    "insert into temp_tair " +
-                    "select hjid, value from ( " +
-                    "select d.entrytype_gene_hjid as hjid, split_part(c.value, '/', " + i + ") as value " +
-                    "from genenametype c INNER JOIN entrytype_genetype d ON (c.entrytype_genetype_name_hjid = d.hjid) " +
-                    "where c.value SIMILAR TO ?) as split_gene " +
-                    "where value SIMILAR TO ? ";
-                    ps = c.prepareStatement(sqlQuery);
-                    ps.setString(1, "%" + tairID + "/%");
-                    ps.setString(2, tairID);
-                    ps.executeUpdate();
-                } catch (SQLException e) {
-                    logSQLException(e, sqlQuery);
-                }
-            }
-
-        // Step 3: extract the genenametype IDs that are likely to be lists of
-        // further IDs, with at least one TAIR ID among them.  For each of these
+        // Step 3: Extract the genenametype IDs; these may be lists of further
+        // IDs, with at least one TAIR ID among them. For each of these
         // potential lists, extract the TAIR IDs, then insert a new individual
         // record for each of them.
         //
-        // FIXME Yes, this is nasty slow.  But more accurate and general.
-//        sqlQuery =
-//            "select d.entrytype_gene_hjid as hjid, c.value " +
-//            "from genenametype c INNER JOIN entrytype_genetype d ON (c.entrytype_genetype_name_hjid = d.hjid) " +
-//            "where c.value SIMILAR TO ? and char_length(c.value) > ? " +
-//            "group by d.entrytype_gene_hjid, c.value";
+        // FIXME Yes, this can be nasty slow on some computers, but it is
+        // accurate and general. In any case, the computer used to develop this
+        // code had satisfactory performance.
+        sqlQuery = "select d.entrytype_gene_hjid as hjid, c.value " +
+            "from genenametype c inner join entrytype_genetype d " +
+            "on (c.entrytype_genetype_name_hjid = d.hjid) " +
+            "where c.value similar to ? " +
+            "group by d.entrytype_gene_hjid, c.value";
 
-        // step 5: Convert all IDs to upper case.
+        // Step 3a: We gather the extracted IDs into memory.
+        List<TAIRPair> ids = new ArrayList<TAIRPair>();
+        try {
+            ps = c.prepareStatement(sqlQuery);
+            // Wildcards on both ends let us select everything that contains
+            // a TAIR ID pattern.
+            ps.setString(1, "%" + tairID + "%");
+            result = ps.executeQuery();
+
+            // genenametype includes "dot-number" TAIR IDs; take these into
+            // account.
+            Pattern p = Pattern.compile(tairID + "(\\.[0-9])?");
+            while (result.next()) {
+                Long hjid = result.getLong("hjid");
+                String idList = result.getString("value");
+                _Log.debug("Processing raw ID: " + idList + " for surrogate " + hjid);
+
+                // Extract matching IDs, including "dot-number" versions.
+                Matcher m = p.matcher(idList);
+                while (m.find()) {
+                    String extractedID = m.group();
+                    _Log.debug("--- Got TAIR ID " + extractedID);
+                    ids.add(new TAIRPair(hjid, extractedID));
+                }
+            }
+        } catch(SQLException e) {
+            logSQLException(e, sqlQuery);
+        }
+
+        // Step 3b: Place the IDs into temp_tair.
+        sqlQuery = "insert into temp_tair(hjid, id) values(?, ?)";
+        try {
+            ps = c.prepareStatement(sqlQuery);
+            for (TAIRPair tp : ids) {
+                ps.setLong(1, tp.hjid);
+                ps.setString(2, tp.tair);
+                _Log.debug("inserting (" + tp.hjid + ", " + tp.tair + ")...");
+                int count = ps.executeUpdate();
+                _Log.debug("...inserted " + count);
+            }
+        } catch(SQLException e) {
+            logSQLException(e, sqlQuery);
+        }
+
+        // Step 5: Convert all IDs to upper case.
+        _Log.debug("Setting all TAIR IDs to uppercase...");
         sqlQuery = "update temp_tair set id = upper(id)";
         try {
             ps = c.prepareStatement(sqlQuery);
             ps.executeUpdate();
-        } catch (SQLException e) {
+        } catch(SQLException e) {
             logSQLException(e, sqlQuery);
+        }
+        // There is no step 4  :-P
+
+        // At this point, temp_tair should be ready for further processing.
+        // Commit and continue.
+        try {
+            c.commit();
+            c.setAutoCommit(priorAutoCommit);
+            _Log.debug("Committed temp TAIR table; all done.");
+        } catch(SQLException e) {
+            logSQLException(e, "Commit and restore prior autocommit");
         }
     }
 
@@ -172,4 +186,17 @@ public class ArabidopsisTAIRIDCollector {
     }
 
     private static final Log _Log = LogFactory.getLog(ArabidopsisTAIRIDCollector.class);
+
+    /**
+     * Helper class for holding a surrogate gene ID and a string equivalent.
+     */
+    private static class TAIRPair {
+        public TAIRPair(Long hjid, String tair) {
+            this.hjid = hjid;
+            this.tair = tair;
+        }
+
+        public Long hjid;
+        public String tair;
+    }
 }
--- trunk/gmbuilder/test/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/ArabidopsisTAIRIDCollectorTest.java
+++ trunk/gmbuilder/test/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/ArabidopsisTAIRIDCollectorTest.java
@@ -1,33 +1,3 @@
-/*
-  ============================================
-  openPACS  - An Open Source PACS Framework
-  ============================================
-
-  Copyright (C) 2007 UCLA Medical Imaging Informatics.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
-  Email: opensource@mii.ucla.edu
-
-  Mail: UCLA Medical Imaging Informatics
-  924 Westwood Bl., Suite 420
-  Westwood, CA 90024
-
-  Web: http://www.mii.ucla.edu/openPACS/
-*/
-// Created by dondi, Jul 13, 2007.
 package edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.profiles;
 
 import java.sql.Connection;

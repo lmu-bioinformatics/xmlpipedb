d2817451bc4d9f6099650e5d7bc1daa99b72d5fc
zugzugglug <zugzugglug@1094f71c-c033-0410-a037-bb4d46bb0a42>
2009-01-08 09:27:08 +0000

    Separated batch files into Vista and non-Vista versions.

---
+++ trunk/gmbuilder/scripts/gmbuilder-vista.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"C:\Program Files (x86)\Java\jre6\bin\java" -D32 -Xmx1024m -jar gmbuilder.jar
--- trunk/gmbuilder/scripts/gmbuilder.bat
+++ trunk/gmbuilder/scripts/gmbuilder.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Program Files (x86)\Java\jre6\bin\java" -D32 -Xmx1024m -jar gmbuilder.jar
+java -Xmx1024m -jar gmbuilder.jar

8372d942807f0383a6acfa22857925386a1e62fd
emontz <emontz@1094f71c-c033-0410-a037-bb4d46bb0a42>
2010-12-01 01:58:47 +0000

    updated to version beta 60

--- trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/GenMAPPBuilder.java
+++ trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/GenMAPPBuilder.java
@@ -81,7 +81,7 @@ public class GenMAPPBuilder extends App implements TallyEngineDelegate {
     /**
      * Version string.
      */
-    public static final String VERSION = "2.0b59";
+    public static final String VERSION = "2.0b60";
 
     /**
      * Starts the application.

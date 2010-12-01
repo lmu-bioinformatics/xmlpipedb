2d41bda2373149bb24dd8b8ffc31cbd1e2f261ab
Forns <Forns@1094f71c-c033-0410-a037-bb4d46bb0a42>
2010-12-01 02:06:14 +0000

    added *probable* fix to the underscore conflict for helicobacter pylori

--- trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/HelicobacterPyloriUniProtSpeciesProfile.java
+++ trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/HelicobacterPyloriUniProtSpeciesProfile.java
@@ -1,7 +1,19 @@
 package edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.profiles;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.ConnectionManager;
 import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager;
 import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager.QueryType;
+import edu.lmu.xmlpipedb.gmbuilder.util.GenMAPPBuilderUtilities;
+import edu.lmu.xmlpipedb.util.exceptions.InvalidParameterException;
 
 public class HelicobacterPyloriUniProtSpeciesProfile extends UniProtSpeciesProfile {
 	
@@ -27,4 +39,63 @@ public class HelicobacterPyloriUniProtSpeciesProfile extends UniProtSpeciesProfi
 	    return tableManager;
 	}
 	
+	
+	/**
+     * @see edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.profiles.UniProtSpeciesProfile#getSystemTableManagerCustomizations(edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager,
+     *      edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager,
+     *      java.util.Date)
+     */
+    @Override
+    public TableManager getSystemTableManagerCustomizations(TableManager tableManager, TableManager primarySystemTableManager, Date version) throws SQLException, InvalidParameterException {
+        // Start with the default OrderedLocusNames behavior.
+        TableManager result = super.getSystemTableManagerCustomizations(tableManager, primarySystemTableManager, version);
+
+        // We want to grab all of the legal OrderedLocusNames Ids and
+        // remove the '_', adding them to the OrderedLocusNames table
+        final String hpID = "HP_*";
+        String sqlQuery = "select d.entrytype_gene_hjid as hjid, c.value " + "from genenametype c inner join entrytype_genetype d " + "on (c.entrytype_genetype_name_hjid = d.hjid) " + "where (c.value similar to ?)" + "group by d.entrytype_gene_hjid, c.value";
+
+        String dateToday = GenMAPPBuilderUtilities.getSystemsDateString(version);
+        Connection c = ConnectionManager.getRelationalDBConnection();
+        PreparedStatement ps;
+        ResultSet rs;
+        try {
+            // Query, iterate, add to table manager.
+            ps = c.prepareStatement(sqlQuery);
+            ps.setString(1, hpID);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String hjid = Long.valueOf(rs.getLong("hjid")).toString();
+
+                // We want to remove the '_' here
+                String id = rs.getString("value");
+                String[] substrings = id.split("/");
+                String new_id = null;
+                for (int i = 0; i < substrings.length; i++) {
+                    new_id = substrings[i].replace("_", "");
+                    _Log.debug("Remove '_' from " + id + " to create: " + new_id + " for surrogate " + hjid);
+                    result.submit("OrderedLocusNames", QueryType.insert, new String[][] { { "ID", new_id }, { "Species", "|" + getSpeciesName() + "|" }, { "\"Date\"", dateToday }, { "UID", hjid } });
+                }
+            }
+        } catch(SQLException sqlexc) {
+            logSQLException(sqlexc, sqlQuery);
+        }
+
+        return result;
+    }
+
+    /**
+     * Helper method for logging an SQL exception.
+     */
+    private void logSQLException(SQLException sqlexc, String sqlQuery) {
+        _Log.error("Exception trying to execute query: " + sqlQuery);
+        while (sqlexc != null) {
+            _Log.error("Error code: [" + sqlexc.getErrorCode() + "]");
+            _Log.error("Error message: [" + sqlexc.getMessage() + "]");
+            _Log.error("Error SQL State: [" + sqlexc.getSQLState() + "]");
+            sqlexc = sqlexc.getNextException();
+        }
+    }
+
+    private static final Log _Log = LogFactory.getLog(HelicobacterPyloriUniProtSpeciesProfile.class);
 }

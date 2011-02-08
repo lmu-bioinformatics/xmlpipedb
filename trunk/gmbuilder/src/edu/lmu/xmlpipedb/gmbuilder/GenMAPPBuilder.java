7a95539e3c34987ff91b1593d39e398b8f6f6339
rbrousla <rbrousla@1094f71c-c033-0410-a037-bb4d46bb0a42>
2011-02-08 01:22:47 +0000

    Updated string constant to ver b61

--- trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/GenMAPPBuilder.java
+++ trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/GenMAPPBuilder.java
@@ -81,7 +81,7 @@ public class GenMAPPBuilder extends App implements TallyEngineDelegate {
     /**
      * Version string.
      */
-    public static final String VERSION = "2.0b60";
+    public static final String VERSION = "2.0b61";
 
     /**
      * Starts the application.

b6883edff916129f77b21b027ff61890133d345e
rbrousla <rbrousla@1094f71c-c033-0410-a037-bb4d46bb0a42>
2011-04-20 20:59:24 +0000

    added taxon id = 246196

--- trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/MycobacteriumSmegmatisUniProtSpeciesProfile.java
+++ trunk/gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/MycobacteriumSmegmatisUniProtSpeciesProfile.java
@@ -5,7 +5,7 @@ import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager.QueryType
 
 public class MycobacteriumSmegmatisUniProtSpeciesProfile extends UniProtSpeciesProfile {
 	public MycobacteriumSmegmatisUniProtSpeciesProfile() {
-		super("Mycobacterium smegmatis (strain ATCC 700084 / mc(2)155)",
+		super("Mycobacterium smegmatis (strain ATCC 700084 / mc(2)155)", 246196, 
 		      "This profile customizes the GenMAPP Builder export for Mycobacterium smegmatis data loaded from a UniProt XML file.");
 	}
 	@Override

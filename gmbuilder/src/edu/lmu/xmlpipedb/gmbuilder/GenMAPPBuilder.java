a6a82747e506646ecfc56f756ea3a3392a7d4453
dondi <dondi@lmu.edu>
2013-10-22 07:09:50 +0000

    Update version number.

--- gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/GenMAPPBuilder.java
+++ gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/GenMAPPBuilder.java
@@ -81,7 +81,7 @@ public class GenMAPPBuilder extends App implements TallyEngineDelegate {
     /**
      * Version string.
      */
-    public static final String VERSION = "2.0b69";
+    public static final String VERSION = "2.0b70";
 
     /**
      * Starts the application.

8fc5b922b7d372672107698c32ddba5d921b4fdc
mrpetredis <mrpetredis@1094f71c-c033-0410-a037-bb4d46bb0a42>
2013-11-14 18:43:54 +0000

    initial version of sinorhizobium meliloti

---
+++ gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/SinorhizobiumMelilotiUniProtSpeciesProfile.java
@@ -0,0 +1,34 @@
+package edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.profiles;
+
+import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager;
+import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager.QueryType;
+
+public class SinorhizobiumMelilotiUniProtSpeciesProfile extends
+		UniProtSpeciesProfile {
+	
+	public SinorhizobiumMelilotiUniProtSpeciesProfile() {
+	    super("Sinorhizobium meliloti",
+	    		266834,
+	        "This profile customizes the GenMAPP Builder export for " +
+	            "Sinorhizobium meliloti" +
+	            " data loaded from a UniProt XML file.");
+
+	}
+	
+	@Override
+	public TableManager getSystemsTableManagerCustomizations(TableManager tableManager, DatabaseProfile dbProfile) {
+	    super.getSystemsTableManagerCustomizations(tableManager, dbProfile);
+	    tableManager.submit("Systems", QueryType.update, new String[][] {
+	        { "SystemCode", "N" },
+	        { "Species", "|" + getSpeciesName() + "|" }
+	    });
+
+	    tableManager.submit("Systems", QueryType.update, new String[][] {
+	        { "SystemCode", "N" },
+	        { "Link", "***species-specific-database-link***" }
+	    });
+
+	    return tableManager;
+	    
+	}
+}

d622c3f1c94eecc7d5f29e14732554c275660057
taur-vil <taur-vil@1094f71c-c033-0410-a037-bb4d46bb0a42>
2013-11-19 18:41:11 +0000

    Added link pattern and changed species link information for TIGR4

--- gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/StreptococcusPneumoniaeTIGR4UniProtSpeciesProfile.java
+++ gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/StreptococcusPneumoniaeTIGR4UniProtSpeciesProfile.java
@@ -3,13 +3,13 @@ package edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.profiles;
 import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager;
 import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager.QueryType;
 
-public class StreptococcusPneumoniaeR6UniProtSpeciesProfile extends
+public class StreptococcusPneumoniaeTIGR4UniProtSpeciesProfile extends
 		UniProtSpeciesProfile {
-	public StreptococcusPneumoniaeR6UniProtSpeciesProfile() {
-	    super("StreptococcusPneumoniaeR6",
-	        171101,
+	public StreptococcusPneumoniaeTIGR4UniProtSpeciesProfile() {
+	    super("StreptococcusPneumoniaeTIGR4",
+	        170187,
 	        "This profile customizes the GenMAPP Builder export for " +
-	            "StreptococcusPneumoniaeR6" +
+	            "StreptococcusPneumoniaeTIGR4" +
 	            " data loaded from a UniProt XML file.");
 	}
 	
@@ -23,7 +23,7 @@ public class StreptococcusPneumoniaeR6UniProtSpeciesProfile extends
 
 	    tableManager.submit("Systems", QueryType.update, new String[][] {
 	        { "SystemCode", "N" },
-	        { "Link", "***species-specific-database-link***" }
+	        { "Link", "http://www.streppneumoniae.com/gene_detail_output.asp?id=2741&name=~" }
 	    });
 
 	    return tableManager;

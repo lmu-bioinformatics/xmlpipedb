9622d1fa3d39336d0cab4be5a97870be835445a3
Jake Woodlee <jwoodlee@lion.lmu.edu>
2015-12-01 15:34:34 -0800

    Custom species profile.

--- gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/GenMAPPBuilder.java
+++ gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/GenMAPPBuilder.java
@@ -80,7 +80,7 @@ public class GenMAPPBuilder extends App implements TallyEngineDelegate {
     /**
      * Version string.
      */
-    public static final String VERSION = "3.0.0-build-5";
+    public static final String VERSION = "3.0.0-build-5_Shigella_flexneri";
 
     /**
      * Starts the application.

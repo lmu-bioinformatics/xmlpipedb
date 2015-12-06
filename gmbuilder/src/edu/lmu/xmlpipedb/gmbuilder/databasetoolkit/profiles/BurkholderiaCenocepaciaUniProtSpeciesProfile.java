83b608c897c7b50fd8cd36894b22817fae934c24
Anu <anu.varshneya@gmail.com>
2015-12-06 12:26:20 -0800

    Customized species profile to collect gene names from ORFs

--- gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/BurkholderiaCenocepaciaUniProtSpeciesProfile.java
+++ gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/BurkholderiaCenocepaciaUniProtSpeciesProfile.java
@@ -1,7 +1,21 @@
 package edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.profiles;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.ConnectionManager;
 import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager;
 import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager.QueryType;
+import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager.Row;
+import edu.lmu.xmlpipedb.util.exceptions.InvalidParameterException;
+
+/**
+ * @author Anindita Varshneya 
+ */
 
 public class BurkholderiaCenocepaciaUniProtSpeciesProfile extends UniProtSpeciesProfile {
 	
@@ -28,4 +42,26 @@ public class BurkholderiaCenocepaciaUniProtSpeciesProfile extends UniProtSpecies
 	
 	    return tableManager;
 	}
+	
+	/**
+	 * Modified code form MycobacteriumTuberculosisUniProtSpeciesProfile.java
+	 */
+    @Override
+    public TableManager getSystemTableManagerCustomizations(TableManager tableManager, TableManager primarySystemTableManager, Date version) throws SQLException, InvalidParameterException {
+        /*
+         * This method is only called (and therefore this bit 'o logic is only
+         * invoked) when the species specific class has not overridden this
+         * method.
+         */
+        List<String> comparisonList = new ArrayList<String>(1);
+        comparisonList.add("ORF");
+
+        return systemTableManagerCustomizationsHelper(tableManager, primarySystemTableManager, version, "OrderedLocusNames", comparisonList);
+    }
+	
+	
+//	"SELECT value, type " +
+//    "FROM genenametype INNER JOIN genetype " +
+//    "ON (genetype_name_hjid = genetype.hjid) " + "WHERE (type = 'ORF' or type = 'ordered locus') and "
+//    		+ "(value like 'p?BCA[]') and entrytype_gene_hjid = ?"
 }

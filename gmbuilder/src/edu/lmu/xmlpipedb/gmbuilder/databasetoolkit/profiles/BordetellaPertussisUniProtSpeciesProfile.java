a3d56b3d0c9b963a5afac6879213a04bbcec14ef
Brandon Klein <brandonklein77@gmail.com>
2015-12-10 16:06:51 -0800

    Created New Build to Export One Missing ID from EnsemblBacteria

--- gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/BordetellaPertussisUniProtSpeciesProfile.java
+++ gmbuilder/src/edu/lmu/xmlpipedb/gmbuilder/databasetoolkit/profiles/BordetellaPertussisUniProtSpeciesProfile.java
@@ -1,10 +1,15 @@
 package edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.profiles;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.ConnectionManager;
 import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager;
 import edu.lmu.xmlpipedb.gmbuilder.databasetoolkit.tables.TableManager.QueryType;
 import edu.lmu.xmlpipedb.util.exceptions.InvalidParameterException;
@@ -36,16 +41,49 @@ public class BordetellaPertussisUniProtSpeciesProfile extends UniProtSpeciesProf
     @Override
     public TableManager getSystemTableManagerCustomizations(TableManager tableManager,
             TableManager primarySystemTableManager, Date version) throws SQLException, InvalidParameterException {
-        /*
-         * This method is only called (and therefore this bit 'o logic is only
-         * invoked) when the species specific class has not overridden this
-         * method.
-         */
-        List<String> comparisonList = new ArrayList<String>(1);
-        comparisonList.add("ordered locus");
-        comparisonList.add("ORF");
+        // Start with the default OrderedLocusNames behavior.
+        TableManager result = super.getSystemTableManagerCustomizations(tableManager, primarySystemTableManager,
+                version);
 
-        return systemTableManagerCustomizationsHelper(tableManager, primarySystemTableManager, version,
-                "OrderedLocusNames", comparisonList);
+        String sqlQuery = "select dbreferencetype.entrytype_dbreference_hjid as hjid, propertytype.value from propertytype inner join dbreferencetype on " +
+                "(propertytype.dbreferencetype_property_hjid = dbreferencetype.hjid) " +
+                "where dbreferencetype.type = 'EnsemblBacteria' and propertytype.type = 'gene ID' " +
+                "and propertytype.value ~ 'BP[0-9][0-9][0-9][0-9](A|B)' order by propertytype.value";
+
+        Connection c = ConnectionManager.getRelationalDBConnection();
+        PreparedStatement ps;
+        ResultSet rs;
+        try {
+            // Query, iterate, add to table manager.
+            ps = c.prepareStatement(sqlQuery);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String hjid = Long.valueOf(rs.getLong("hjid")).toString();
+                String id = rs.getString("value");
+                result.submit("OrderedLocusNames", QueryType.insert, new Object[][] {
+                    { "ID", id },
+                    { "Species", "|" + getSpeciesName() + "|" },
+                    { "Date", version },
+                    { "UID", hjid }
+                });
+            }
+        } catch(SQLException sqlexc) {
+            logSQLException(sqlexc, sqlQuery);
+        }
+
+        return result;
     }
+
+    private void logSQLException(SQLException sqlexc, String sqlQuery) {
+        LOG.error("Exception trying to execute query: " + sqlQuery);
+        while (sqlexc != null) {
+            LOG.error("Error code: [" + sqlexc.getErrorCode() + "]");
+            LOG.error("Error message: [" + sqlexc.getMessage() + "]");
+            LOG.error("Error SQL State: [" + sqlexc.getSQLState() + "]");
+            sqlexc = sqlexc.getNextException();
+        }
+    }
+
+    private static final Log LOG = LogFactory.getLog(BordetellaPertussisUniProtSpeciesProfile.class);
+
 }

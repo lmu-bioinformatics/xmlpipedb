56664dcfb870dffd9776e2b3afec992c1e6e39db
dondi <dondi@lmu.edu>
2015-12-12 15:02:51 -0800

    Revert "Patch 1"

--- gmbuilder/sql/special-case-query.sql
+++
@@ -1,4 +0,0 @@
-select propertytype.value from propertytype inner join dbreferencetype on
-    (propertytype.dbreferencetype_property_hjid = dbreferencetype.hjid)
-    where dbreferencetype.type = 'EnsemblBacteria' and dbreferencetype.id ~ 'AAN[0-9][0-9][0-9][0-9][0-9]' and propertytype.type = 'gene ID' 
-    and propertytype.value ~ 'SF[0-9][0-9][0-9][0-9]' order by propertytype.value;
